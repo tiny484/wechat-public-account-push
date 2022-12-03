be2705cdeb082954d0605242e989b864e24326bd
tiny <49485640+tiny484@users.noreply.github.com>
2022-12-03 19:54:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,9 +19,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: 'Tiny',
+      name: '扶南',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oI7L86k1GVJuOUB3vg0EvijK7BRw',
+      id: 'oI7L86s9sXF9COcsxHc6LbaH8_qk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '9xHRa0TBuJIDobAHyh5Vrm5fIbnzPVtPsEvRldTBuCU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
