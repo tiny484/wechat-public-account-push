04fe60196223a69da0786112cf0926db661278f9
tiny <49485640+tiny484@users.noreply.github.com>
2022-12-03 19:53:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -48,7 +48,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-//   CALLBACK_TEMPLATE_ID: '4YoX2KjNHyAwYmOAoWYpw8g-wfhbHmRxkNrqJlKY49I',
+  CALLBACK_TEMPLATE_ID: '9xHRa0TBuJIDobAHyh5Vrm5fIbnzPVtPsEvRldTBuCU',
 
   CALLBACK_USERS: [
     {
